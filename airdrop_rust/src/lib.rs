c21948bdd59d6b6b635312a591a4042aa7799d31
ritikbhatt20 <ritikbhatt020@gmail.com>
2024-07-26 05:36:30 +0530

    Feat: Initialized the airdrop project using rust

---
+++ airdrop_rust/src/lib.rs
@@ -0,0 +1,17 @@
+#[cfg(test)]
+mod tests {
+    use solana_sdk::{signature::{Keypair, Signer}, pubkey::Pubkey};
+    #[test]
+    fn keygen() {
+        // Create a new keypair
+        let kp = Keypair::new();
+        println!("You've generated a new Solana wallet: {}", kp.pubkey().to_string());
+        println!("");
+        println!("To save your wallet, copy and paste the following into a JSON file:");
+        println!("{:?}", kp.to_bytes());
+    }
+    #[test]
+    fn airdop() {}
+    #[test]
+    fn transfer_sol() {}
+}
