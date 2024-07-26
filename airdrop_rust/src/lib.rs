f1ee93e4befe7e7bf2cb5502bced1dc77dc90488
ritikbhatt20 <ritikbhatt020@gmail.com>
2024-07-26 06:10:36 +0530

    Feat: Successfully airdropped

--- airdrop_rust/src/lib.rs
+++ airdrop_rust/src/lib.rs
@@ -1,6 +1,15 @@
 #[cfg(test)]
 mod tests {
-    use solana_sdk::{signature::{Keypair, Signer}, pubkey::Pubkey};
+    use solana_sdk::{
+        signature::{Keypair, Signer, read_keypair_file},
+        pubkey::Pubkey,
+    };
+    use solana_client::rpc_client::RpcClient;
+    use bs58;
+    use std::io::{self, BufRead};
+
+    const RPC_URL: &str = "https://api.devnet.solana.com";
+
     #[test]
     fn keygen() {
         // Create a new keypair
@@ -10,8 +19,55 @@ mod tests {
         println!("To save your wallet, copy and paste the following into a JSON file:");
         println!("{:?}", kp.to_bytes());
     }
+
     #[test]
-    fn airdop() {}
+    fn base58_to_wallet() {
+        println!("Input your private key as base58:");
+        let stdin = io::stdin();
+        let base58 = stdin.lock().lines().next().unwrap().unwrap();
+        println!("Your wallet file is:");
+        let wallet = bs58::decode(base58).into_vec().unwrap();
+        println!("{:?}", wallet);
+    }
+
+    #[test]
+    fn wallet_to_base58() {
+        println!("Input your private key as a wallet file byte array:");
+        let stdin = io::stdin();
+        let wallet = stdin
+            .lock()
+            .lines()
+            .next()
+            .unwrap()
+            .unwrap()
+            .trim_start_matches('[')
+            .trim_end_matches(']')
+            .split(',')
+            .map(|s| s.trim().parse::<u8>().unwrap())
+            .collect::<Vec<u8>>();
+        println!("Your private key is:");
+        let base58 = bs58::encode(wallet).into_string();
+        println!("{:?}", base58);
+    }
+
+    #[test]
+    fn airdop() {
+        // Import our keypair
+        let keypair = read_keypair_file("dev-wallet.json").expect("Couldn't find wallet file");
+
+        // Connect to Solana Devnet RPC Client
+        let client = RpcClient::new(RPC_URL);
+
+        // We're going to claim 2 devnet SOL tokens (2 billion lamports)
+        match client.request_airdrop(&keypair.pubkey(), 2_000_000_000u64) {
+            Ok(s) => {
+                println!("Success! Check out your TX here:");
+                println!("https://explorer.solana.com/tx/{}?cluster=devnet", s.to_string());
+            },
+            Err(e) => println!("Oops, something went wrong: {}", e.to_string()),
+        }
+    }
+
     #[test]
     fn transfer_sol() {}
 }
